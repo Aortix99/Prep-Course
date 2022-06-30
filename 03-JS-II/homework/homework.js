acdfca6b39cb3590a69a2ddc663dd4fa77535ec7
unknown <acocogollo@gmail..com>
2022-06-29 22:28:34 -0500

    super suave

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -113,13 +113,24 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  tab=1
+  a=[]
+  for(i=0; i<=60; i++){
+    tab=6*i
+    a.push(tab)
+  }return a
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  a=""
+  if(numero.toString().length==3){
+    a="este entero tiene tres digitos"
+  }else{
+    a=false
+  }
+     return a
 }
 
 function doWhile(numero) {
